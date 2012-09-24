5ccde5a555fec421497501cd832583daefd7310c
Arthur Gouveia <arthur.gouveia@gmail.com>
2012-09-24 11:59:56 -0300

    First commit

---
+++ prettyCheckable.js
@@ -0,0 +1,115 @@
+/*
+ *  Project: prettyCheckable
+ *  Description: jQuery plugin to replace checkboxes and radios for custom images
+ *  Author: Arthur Gouveia
+ *  License: Licensed under the MIT License
+ */
+
+;(function ( $, window, undefined ) {
+
+    var pluginName = 'prettyCheckable',
+      document = window.document,
+      defaults = {
+        labelPosition: 'labelright',
+        customClass: '',
+        color: 'blue'
+      };
+
+    function Plugin( element, options ) {
+      this.element = element;
+      this.options = $.extend( {}, defaults, options) ;
+
+      this._defaults = defaults;
+      this._name = pluginName;
+
+      this.init();
+    }
+
+    function loadCheckBoxEvents(){
+
+      var hitEvent = 'ontouchstart' in document.documentElement ? 'touchstart' : 'click';
+
+      $('div.prettycheckbox a, div.prettyradio a, div.prettycheckbox label, div.prettyradio label').on(hitEvent, function(e){
+
+        e.preventDefault();
+
+        var clickedParent = $(this).closest('.clearfix');
+        var input = clickedParent.find('input');
+        var fakeCheckable = clickedParent.find('a');
+
+        if (input.attr('type') == 'radio') {
+
+          $('input[name=' + input.attr('name') + ']').each(function(index, el){
+            $(el).removeAttr('checked').parent().find('a').removeClass('checked');
+          });
+
+        }
+
+        if (input.attr('checked') !== undefined) {
+
+          input.removeAttr('checked');
+
+        } else {
+
+          input.attr('checked', 'checked').change();
+
+        }
+
+        fakeCheckable.toggleClass('checked');
+
+      });
+
+      $('div.prettycheckbox a, div.prettyradio a').on('keypress', function(){ $(this).click(); });
+
+    }
+
+    Plugin.prototype.init = function () {
+
+      el = $(this.element);
+
+      el.css('display', 'none');
+
+      var classType = el.data('type') !== undefined ? el.data('type') : el.attr('type');
+
+      var label = el.data('label') !== undefined ? el.data('label') : '';
+
+      var labelPosition = el.data('labelposition') !== undefined ? 'label' + el.data('labelposition') : 'label' + this.options.labelPosition;
+
+      var customClass = el.data('customclass') !== undefined ? el.data('customclass') : this.options.customClass;
+
+      var color =  el.data('color') !== undefined ? el.data('color') : this.options.color;
+
+      var containerClasses = ['pretty' + classType, labelPosition, customClass, color].join(' ');
+
+      el.wrap('<div class="clearfix ' + containerClasses + '"></div>').parent().html();
+      
+      var dom = [];
+      var isChecked = el.attr('checked') !== undefined ? 'checked' : '';
+
+      if (labelPosition === 'labelright') {
+
+        dom.push('<a href="#" class="' + isChecked + '"></a>');
+        dom.push('<label for="' + el.attr('id') + '">' + label + '</label>');
+
+      } else {
+
+        dom.push('<label for="' + el.attr('id') + '">' + label + '</label>');
+        dom.push('<a href="#" class="' + isChecked + '"></a>');
+
+      }
+
+      el.parent().append(dom.join('\n'));
+
+    };
+
+    $.fn[pluginName] = function ( options ) {
+      this.each(function () {
+        if (!$.data(this, 'plugin_' + pluginName)) {
+          $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+        }
+      });
+      loadCheckBoxEvents();
+      return this;
+    };
+
+}(jQuery, window));
